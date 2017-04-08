8f89d92a1e0f703773c9e52cfaae853c1ee311ab
bingnuoyuan <412896819@qq.com>
2017-04-08 15:33:26 +0800

    editor

--- dist/js/jsoneditortree.js
+++ dist/js/jsoneditortree.js
@@ -5,13 +5,34 @@
 		options = $.extend({},options)
 		, this.elem = elem
 		, this.options = options
-
+		, this.tree = ''
 		this.init()
 	}
 	jsonEditorTree.prototype = {
 		// body...
 		init:function(){
-			console.log('init')
+			var schema = this.options.schema
+			var  html = this.createNode(schema)
+			this.elem.append(html)
+		}
+		, createNode:function(schema){
+			this.tree += '<p data-type="'+schema.type+'">'+schema.title+'</p>'
+			if(schema.properties){ //obj
+				this.tree += this.createObj(schema.properties)
+			}
+			return this.tree 
+		}
+		, createObj:function(nodeDate){
+			$.each(nodeDate,function(i,elem){
+				console.log(i,elem)
+				if(elem.type == "object"){
+					this.tree += '<p data-type="'+elem.type+'">'+elem.title+'</p>'
+					if(elem.properties != undefiend){
+						this.tree +=  this.createObj(elem.properties)
+					}
+				}
+			})
+			return this.tree
 		}
 	};
 	$.fn.jsoneditortree = function(elem,options){
